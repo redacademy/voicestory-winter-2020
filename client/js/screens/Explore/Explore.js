66f6279ef3bc25a5cda940fee21b6b0112a570b7
Ken Huang <khuang.dev@gmail.com>
2020-03-16 11:22:06 -0700

    add basic component to screens, install react-navigation

---
+++ client/js/screens/Explore/Explore.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Explore = () => {
+  return <Text>Explore Page</Text>;
+};
+
+export default Explore;
---
+++ client/js/screens/Explore/ExploreContainer.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import Explore from './Explore';
+
+export default class ExploreContainer extends Component {
+  render() {
+    return <Explore />;
+  }
+}
