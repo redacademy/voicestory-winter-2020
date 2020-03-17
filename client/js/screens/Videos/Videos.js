2b3b50043f04cca572bb1d5eaf9684a48e2a627e
Ken Huang <khuang.dev@gmail.com>
2020-03-16 20:08:32 -0700

    update Explore and Videos screen styling

--- client/js/screens/Videos/Videos.js
+++ client/js/screens/Videos/Videos.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import {Text, TouchableOpacity, View} from 'react-native';
+import styles from './styles';
 
 const Videos = ({navigation}) => {
   return (
     <>
-      <TouchableOpacity>
-        <Text>Newest</Text>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Text>Most Viewed</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('Theme')}>
-        <Text>Theme</Text>
-      </TouchableOpacity>
+      <View style={styles.filterContainer}>
+        <TouchableOpacity>
+          <Text>Newest</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text>Most Viewed</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('Theme')}>
+          <Text>Theme</Text>
+        </TouchableOpacity>
+      </View>
     </>
   );
 };
