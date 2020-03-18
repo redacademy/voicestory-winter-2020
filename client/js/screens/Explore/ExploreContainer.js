d5e07a12e728a9173aede84015ba6f2447d7bb29
Kieran Devlin <kjrdevlin@gmail.com>
2020-03-18 09:26:52 -0700

    Fix unresolved merge conflict

--- client/js/screens/Explore/ExploreContainer.js
+++ client/js/screens/Explore/ExploreContainer.js
@@ -2,12 +2,6 @@ import React, {Component} from 'react';
 import Explore from './Explore';
 
 export default class ExploreContainer extends Component {
-<<<<<<< HEAD
-=======
-  constructor(props) {
-    super(props);
-  }
->>>>>>> develop
   render() {
     return (
       <Explore navigation={this.props.navigation} route={this.props.route} />
