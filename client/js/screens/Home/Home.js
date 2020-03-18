80916ba5baee6bc7f1568f16ef1f465bae78d882
Ivan <ivandaixivwork@gmail.com>
2020-03-18 11:42:35 -0700

    Adds toggling between signup and login form

--- client/js/screens/Home/Home.js
+++ client/js/screens/Home/Home.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import LoginForm from '../../components/LoginForm';
 import {View} from 'react-native';
 import SignUpForm from '../../components/SignUpForm';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import Text from '../../components/CustomText/CustomText';
+import styles from './styles';
 
 const Home = () => {
-  return <SignUpForm />;
+  const [formToggle, setFormToggle] = useState(true);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.toggleContainer}>
+        <TouchableOpacity
+          onPress={() => {
+            setFormToggle(true);
+          }}
+          style={formToggle && styles.borderBottom}>
+          <Text style={formToggle ? styles.underline : styles.formToggle}>
+            Sign Up
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            setFormToggle(false);
+          }}
+          style={!formToggle && styles.borderBottom}>
+          <Text style={!formToggle ? styles.underline : styles.formToggle}>
+            Login
+          </Text>
+        </TouchableOpacity>
+      </View>
+      {formToggle ? <SignUpForm /> : <LoginForm />}
+    </View>
+  );
 };
 
 export default Home;
--- client/js/screens/Home/styles.js
+++ client/js/screens/Home/styles.js
@@ -5,6 +5,24 @@ const styles = StyleSheet.create({
     height: Dimensions.get('window').height,
     justifyContent: 'center',
   },
+  toggleContainer: {
+    width: 150,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignSelf: 'center',
+  },
+  formToggle: {
+    fontSize: 18,
+  },
+  underline: {
+    fontSize: 18,
+    fontWeight: '500',
+  },
+  borderBottom: {
+    borderStyle: 'solid',
+    borderBottomWidth: 2,
+    borderBottomColor: '#DB4F48',
+  },
 });
 
 export default styles;
