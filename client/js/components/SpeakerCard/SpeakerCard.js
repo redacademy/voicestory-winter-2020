5dbb5a7df6566c61fe5ad13b4c5b74c47ad020c6
Ian <55965026+IED92@users.noreply.github.com>
2020-03-20 11:40:13 -0700

    Revert "Added linkage to speaker profile from speaker card"
    This reverts commit ab005c74d55fac6af0b298fdb4860083cb52b6d3.

--- client/js/components/SpeakerCard/SpeakerCard.js
+++ client/js/components/SpeakerCard/SpeakerCard.js
@@ -4,26 +4,17 @@ import styles from './styles';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import Text from '../CustomText/CustomText';
 
-const SpeakerCard = ({speaker, style, navigation}) => {
-  const speakerProfilePic =
-    speaker && speaker.profile_picture
-      ? {uri: speaker.profile_picture}
-      : {uri: 'https://placedog.net/500'};
-  const speakerName =
-    speaker && speaker.owner.name ? speaker.owner.name : 'Name';
-  console.log(speaker);
+const SpeakerCard = ({name, style, source}) => {
   return (
-    <TouchableOpacity
-      onPress={() => navigation.navigate('SpeakerProfile', {speaker: speaker})}
-      style={[styles.button, style]}>
+    <TouchableOpacity style={[styles.button, style]}>
       <Image
         style={{height: '75%', width: '100%'}}
         resizeMode={'cover'}
-        source={speakerProfilePic}
+        source={source}
         borderTopLeftRadius={10}
         borderTopRightRadius={10}
       />
-      <Text style={styles.label}>{speakerName}</Text>
+      <Text style={styles.label}>{name}</Text>
     </TouchableOpacity>
   );
 };
