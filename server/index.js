ac155b0a79c7be060c63c654353e7a3ccdd6213f
Ivan <ivandaixivwork@gmail.com>
2020-03-20 16:14:45 -0700

    Adds login functionality to the resolver

--- server/index.js
+++ server/index.js
@@ -1,29 +1,34 @@
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
-
 const { prisma } = require("./generated/prisma-client/index");
+const jwt = require("jsonwebtoken");
+
+const getUser = token => {
+  try {
+    if (token) {
+      return jwt.verify(token, "my-secret-from-env-file-in-prod");
+    }
+    return null;
+  } catch (err) {
+    return null;
+  }
+};
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: {
-    prisma
+  context: ({ req }) => {
+    const tokenWithBearer = req.headers.authorization || "";
+    const token = tokenWithBearer.split(" ")[1];
+    const user = getUser(token);
+
+    return {
+      user,
+      prisma // the generated prisma client if you are using it
+    };
   }
 });
-// const server = new ApolloServer({
-//     typeDefs,
-//     resolvers,
-//     context: ({ req }) => {
-//       const tokenWithBearer = req.headers.authorization || “”;
-//       const token = tokenWithBearer.split(” “)[1];
-//       const user = getUser(token);
-//       return {
-//         user,
-//         prisma
-//       };
-//     }
-//   });
 server
   .listen({
     port: 8383
--- server/resolvers.js
+++ server/resolvers.js
@@ -1,13 +1,9 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const resolvers = {
   Mutation: {
-    register: async (
-      parent,
-      { username, password, name, email },
-      ctx,
-      info
-    ) => {
+    register: async (parent, { username, password }, ctx, info) => {
       const hashedPassword = await bcrypt.hash(password, 10);
       const user = await ctx.prisma.createUser({
         username,
@@ -16,6 +12,34 @@ const resolvers = {
         email: "testemailagain@gmail.com"
       });
       return user;
+    },
+    login: async (parent, { username, password }, ctx, info) => {
+      const user = await ctx.prisma.user({ username });
+
+      if (!user) {
+        throw new Error("Invalid Login");
+      }
+
+      const passwordMatch = await bcrypt.compare(password, user.password);
+
+      if (!passwordMatch) {
+        throw new Error("Invalid Login");
+      }
+
+      const token = jwt.sign(
+        {
+          id: user.id,
+          username: user.email
+        },
+        "my-secret-from-env-file-in-prod",
+        {
+          expiresIn: "30d" // token will expire in 30days
+        }
+      );
+      return {
+        token,
+        user
+      };
     }
   }
 };
