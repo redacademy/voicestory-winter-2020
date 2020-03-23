fa2d83d95ab271d4e04a74a9370ac3f00baa6c0d
Ivan <ivandaixivwork@gmail.com>
2020-03-23 16:19:24 -0700

    Adds display all the speaker's names

--- client/js/screens/Speakers/Speakers.js
+++ client/js/screens/Speakers/Speakers.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import {TouchableOpacity, View} from 'react-native';
 import Text from '../../components/CustomText/CustomText';
+import {SectionList} from 'react-native';
+
+const formatSessionData = users => {
+  return users
+    .reduce((acc, curr) => {
+      const nameExists = acc.find(section => section.title === curr.name);
+      nameExists
+        ? nameExists.data.push(curr)
+        : acc.push({title: curr.name, data: [curr]});
+      return acc;
+    }, [])
+    .sort((a, b) => a.title - b.title);
+};
 
 const Speakers = ({users, navigation}) => {
   users = users.filter(user => user.isSpeaker != null);
   console.log('Speakers', users);
   return (
-    <>
-      <TouchableOpacity onPress={() => navigation.navigate('SpeakerProfile')}>
-        <Text>Speaker</Text>
-      </TouchableOpacity>
-    </>
+    users && (
+      <>
+        <SectionList
+          sections={formatSessionData(users)}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => <Text>{item.title}</Text>}
+          renderSectionHeader={({section: {title}}) => <Text>{title}</Text>}
+        />
+        <TouchableOpacity onPress={() => navigation.navigate('SpeakerProfile')}>
+          <Text>Speaker</Text>
+        </TouchableOpacity>
+      </>
+    )
   );
 };
 
