9c925e23620e2736f8dfcce3f4f362837434fa18
Ivan <ivandaixivwork@gmail.com>
2020-03-31 16:40:21 -0700

    Adds proptype checking for user

--- client/js/screens/UserProfile/UserProfile.js
+++ client/js/screens/UserProfile/UserProfile.js
@@ -4,6 +4,7 @@ import {TouchableOpacity, View} from 'react-native';
 import styles from './styles';
 import Button from '../../components/Button';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import PropTypes from 'prop-types';
 
 const UserProfile = ({navigation, user}) => {
   return (
@@ -41,6 +42,10 @@ const UserProfile = ({navigation, user}) => {
       </View>
     </View>
   );
+};
+UserProfile.propTypes = {
+  navigation: PropTypes.objectOf(PropTypes.func),
+  user: PropTypes.object,
 };
 
 export default UserProfile;
